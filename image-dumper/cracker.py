96e9e4fcf0201b172fa4aeedcb5cd46630560711
maehw <maehw@posteo.de>
2024-08-25 18:44:23 +0200

    Add verbose output, add sanity check of binary blob, add comments to source code and also update README.md with known configurations

--- image-dumper/cracker.py
+++ image-dumper/cracker.py
@@ -5,7 +5,11 @@ import struct
 app_path = os.path.join(os.getcwd(), 'radare2', 'bin')
 os.environ["PATH"] += os.pathsep + app_path
 
+# the patches dictionary contains x86 opcodes to be replaced at pre-known locations;
+# this is where the magic happens
 patches = {
+    # 0x90: Mnemonic "NOP" ("No Operation")
+    # 0xEB: Mnemonic "JMP rel8" ("Short Relative Jump")
     0x4136EA: 0x90,
     0x4136EB: 0x90,
     0x4136EC: 0x90,
@@ -24,26 +28,39 @@ patches = {
 }
 
 import r2pipe
+# load the PED Basic Windows executable with r2
+print("Loading executable...")
 r = r2pipe.open('pelite.exe', ['-w'])
 
+# run multiple r2 commands (returns the output of the command executions)
+# "ood": reopen in debug mode
+# "dc": continue execution of all children
+print("Reopening in debug mode and continuing execution...")
 print(r.cmd("ood; dc; dc;"))
 
+# list symbols of target lib ("dmi")
 relocs = r.cmd("dmij")
+print("Printing target library symbols...")
 print(relocs)
 relocs = relocs.split("\n")
 cardios = [line for line in relocs if "CardIO.dll" in line]
 cardio_addr = int(re.findall(r"0x([0-9A-Fa-f]+)", cardios[-1])[0],16)
 
+print("Applying patches...")
 for patch_addr, patch_val in patches.items():
-    if (patch_addr > 0x10000000):
+    if patch_addr > 0x10000000:
         patch_addr -= 0x10000000
         patch_addr += cardio_addr
-    
+
+    # apply patches by writing known values at known addresses ("w [str] [@addr]")
     cmd = "w \\x{:x} @ {}".format(patch_val, patch_addr)
     print(cmd)
     r.cmd(cmd)
 
 offset = cardio_addr + 0x6ad2
+# continue until address ("dcu")
+print("Continuing. GUI should open...")
+print("Output here will continue after card download attempt. Have fun!")
 r.cmd("dcu {}".format(offset))
 
 # r2pipe has some weird buffering issue with json commands and 
@@ -51,10 +68,12 @@ r.cmd("dcu {}".format(offset))
 mem = None
 for i in range(5):
     mem = r.cmdj("p8j 4 @ rcx")
-    if (mem != None and len(mem) > 0):
+    if mem is not None and len(mem) > 0:
         break
  
-    
+
+# we get here after performing a write operation to the card
+print("Sending to virtual embroidery card. Printing memory...")
 print(mem)
 mem = struct.pack("4B", *mem)
 print(mem)
@@ -62,15 +81,29 @@ mem = int.from_bytes(mem, "little")
 print(mem)
 
 offset = cardio_addr + 0x6b0e
+# continue until that address
 r.cmd("dcu {}".format(offset))
 
 # Ditto
 data = None
 for i in range(5):
     data = bytes(r.cmdj("pxj 0x10000 @ {}".format(mem)))
-    if (data != None and len(data) > 0):
+    if data is not None and len(data) > 0:
         break
 
-f = open("image.bin", "wb")
-f.write(data)
-f.close()
+# got all the binary data
+print("Got {} bytes of card data.".format(len(data)))
+
+# perform a sanity check: look for expected data in the whole blob
+magic_string_expected = b'created by PED-Basic'
+magic_string_read = data[50:70]
+if magic_string_read == magic_string_expected:
+    # looks good, so let's finally store it as a binary file
+    print("Writing output to binary file...")
+    f = open("image.bin", "wb")
+    f.write(data)
+    f.close()
+    print("Done.")
+else:
+    print("Expected '{}' but got '{}'.".format(magic_string_expected.decode("ascii"),
+                                               magic_string_read.decode("ascii")))
